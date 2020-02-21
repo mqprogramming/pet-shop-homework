6d84355859d2a168333786ce6607e6c7e1d8f226
mqprogramming <matt.qgley@gmail.com>
2020-02-21 21:54:30 +0000

    extension work

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -45,3 +45,61 @@ def find_pet_by_name(pet_shop, a_name)
   return found_name
 
 end
+
+def remove_pet_by_name(pet_shop, a_name)
+
+  pet_shop[:pets].each_with_index() do |pet, index|
+
+    if( pet[:name] == a_name )
+      pet_shop[:pets].delete_at(index)
+    end
+
+  end
+
+end
+
+def add_pet_to_stock(pet_shop, new_pet)
+  pet_shop[:pets].push(new_pet)
+end
+
+def customer_cash(customer)
+  return customer[:cash]
+end
+
+def remove_customer_cash(customer, amount)
+  customer[:cash] -= amount
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].count()
+end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push(new_pet)
+end
+
+def customer_can_afford_pet(customer, new_pet)
+
+  if( customer[:cash] >= new_pet[:price] )
+    return true
+  else
+    return false
+  end
+
+end
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+
+  customer_can_afford_pet(customer, pet)
+  add_pet_to_customer(customer, pet)
+  remove_pet_by_name(pet_shop, pet)
+
+  amount = pet_shop[:pets].find() do |a_pet|
+    pet[:name] == a_pet
+  end
+
+
+
+  remove_customer_cash(customer, )
+
+end
