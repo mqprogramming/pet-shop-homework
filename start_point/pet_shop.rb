1022695325ab71c656f17ab4d7812b948e8037f9
mqprogramming <matt.qgley@gmail.com>
2020-02-21 16:19:39 +0000

    third commit.5

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,3 +1,27 @@
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, cash)
+  pet_shop[:admin][:total_cash] += cash
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, added_num)
+  pet_shop[:admin][:pets_sold] += added_num
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].count()
+end
+
+def pets_by_breed(pet_shop, specific_breed)
+  return pet_shop[:pets]
+end
